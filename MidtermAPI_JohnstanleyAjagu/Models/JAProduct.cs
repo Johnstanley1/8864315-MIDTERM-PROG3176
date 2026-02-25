490bb9770b6f9b104b8ac9f3cd358532672b0bed
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2026-02-25 17:07:59 -0500

    Create project

---
+++ MidtermAPI_JohnstanleyAjagu/Models/JAProduct.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MidtermAPI_JohnstanleyAjagu.Models
+{
+    public class JAProduct
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1,25)]
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
